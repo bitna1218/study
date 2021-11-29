14cefb0a3b33ff8d114a48651adb0f5ed3b76a6d
bitna1218 <hbn1218@naver.com>
2021-11-29 22:54:52 +0900

    11월29일

---
+++ out/production/java_projact/com/company/연습문제4_1.java
@@ -0,0 +1,29 @@
+
+public class 연습문제4_1 {public static void main(String[] args) {
+    /*
+    int x = 11;
+    if(10<x && x<20){
+        System.out.print("true");
+    }
+    */
+
+    /*
+    char ch = 'x';
+    if(ch=='x' || ch=='X'){
+        System.out.print("true");
+    }*/
+
+    /*
+    char ch = 'a';
+    if('a'<=ch&&ch<='z')||('A'<=ch&&ch<='Z'){
+        System.out.print("true");
+    }*/
+
+
+    int year=22;
+    if(year%400==0||year%4==0&& year%100!=0){
+        System.out.print("true");
+    }
+}
+}
+
