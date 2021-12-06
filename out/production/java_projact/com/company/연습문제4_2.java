2f6ceec1bbcaf5c19c497146eb003f77d57900c7
bitna1218 <hbn1218@naver.com>
2021-12-06 22:30:00 +0900

    12월6일

---
+++ out/production/java_projact/com/company/연습문제4_2.java
@@ -0,0 +1,9 @@
+public class 연습문제4_2 {public static void main(String[] args){
+    int sum =0;
+    for(int i=1;i<=5;i++){
+        if(i%2!=0 && i%3!=0);
+            sum+=i;
+    }
+    System.out.println(sum);
+}
+}
