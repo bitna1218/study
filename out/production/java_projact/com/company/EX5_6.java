6aab2ba0c9144b9fb09d0f12f6d86b1516e76af6
bitna1218 <hbn1218@naver.com>
2021-12-10 09:05:10 +0900

    12월09일 스터디

---
+++ out/production/java_projact/com/company/EX5_6.java
@@ -0,0 +1,14 @@
+
+public class EX5_6 {public static void main(String[] args){
+    String[]names={"Kim","cho","Han"};
+
+    for(int i=0;i<names.length;i++)
+        System.out.println("names["+i+"]:"+names[i]);
+    String tmp =names[2];
+    System.out.println("tmp:"+tmp);
+    names[0]="Yu";
+
+    for(int i=0;i<names.length;i++)
+        System.out.println("["+i+"]"+names[i]);
+}
+}
